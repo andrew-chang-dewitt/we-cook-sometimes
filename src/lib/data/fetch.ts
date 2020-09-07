68e97cfaaf13bc6d5add5cf44685ef26349729e3
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-09-07 00:12:51 -0400

    inline docs

--- src/lib/data/fetch.ts
+++ src/lib/data/fetch.ts
@@ -1,6 +1,7 @@
 export const trelloFetch = (endpoint: string) => {
   const root = 'https://api.trello.com/1/'
 
+  // guard against fetch being undefined for server-side calls
   let fetch
 
   if (!fetch) {
