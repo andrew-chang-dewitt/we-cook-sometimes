722028c079956ed14b425ff20305911f56ea7b87
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-09-08 21:24:49 -0400

    adding image, recipe details, & tags functions

--- src/lib/data/fetch.spec.ts
+++ src/lib/data/fetch.spec.ts
@@ -3,14 +3,13 @@ import { expect } from 'chai'
 import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 
-import { trello } from './fetch'
+import { recipes, tags, details, image, Tag, Recipe } from './fetch'
 
-describe('lib/Fetch', () => {
-  const server = setupServer(
-    rest.get('https://api.trello.com/1/test', (_, res, ctx) => {
-      return res(ctx.json({ data: 'This is Fetch' }))
-    })
-  )
+describe('lib/data/fetch', () => {
+  const root = 'https://api.trello.com/1'
+  const board = '/board/5820f9c22043447d3f4fa857'
+
+  const server = setupServer()
 
   before(() => {
     server.listen()
@@ -22,7 +21,285 @@ describe('lib/Fetch', () => {
     server.close()
   })
 
-  it('returns data from a given API endpoint', async () => {
-    expect(await trello('test')).to.deep.equal({ data: 'This is Fetch' })
+  describe('image()', () => {
+    const imgObj = {
+      id: '1',
+      name: 'name',
+      edgeColor: 'color',
+      url: 'url',
+      previews: [
+        {
+          height: 1,
+          width: 1,
+          url: 'url1',
+        },
+        {
+          height: 10,
+          width: 10,
+          url: 'url10',
+        },
+        {
+          height: 100,
+          width: 100,
+          url: 'url100',
+        },
+      ],
+    }
+
+    beforeEach(() => {
+      server.use(
+        rest.get(root + '/card/1/attachments/1', (_, res, ctx) =>
+          res(ctx.json(imgObj))
+        )
+      )
+    })
+
+    it('returns an Image object for a given recipe & image ID', async () => {
+      expect(await image('1', '1')).to.deep.equal({
+        url: 'url',
+        id: imgObj.id,
+        name: imgObj.name,
+        edgeColor: imgObj.edgeColor,
+      })
+    })
+
+    it('can return the smallest scaled image that is still >= the optionally given dimensions', async () => {
+      expect(await image('1', '1', { height: 9, width: 9 })).to.deep.equal({
+        url: 'url10',
+        id: imgObj.id,
+        name: imgObj.name,
+        edgeColor: imgObj.edgeColor,
+      })
+    })
+
+    it('only requires a min height or a width to be specified', async () => {
+      expect(await image('1', '1', { height: 100 })).to.deep.equal({
+        url: 'url100',
+        id: imgObj.id,
+        name: imgObj.name,
+        edgeColor: imgObj.edgeColor,
+      })
+    })
+
+    it('must be >= both, if two dimensions are given', async () => {
+      expect(await image('1', '1', { height: 1, width: 100 })).to.deep.equal({
+        url: 'url100',
+        id: imgObj.id,
+        name: imgObj.name,
+        edgeColor: imgObj.edgeColor,
+      })
+    })
+
+    it("returns the largest available, if there isn't one any bigger", async () => {
+      expect(await image('1', '1', { height: 101, width: 101 })).to.deep.equal({
+        url: 'url100',
+        id: imgObj.id,
+        name: imgObj.name,
+        edgeColor: imgObj.edgeColor,
+      })
+    })
+  })
+
+  it('tags() returns a list of labels for the board', async () => {
+    const labels = [('a label' as any) as Tag]
+    server.use(
+      rest.get(root + board + '/labels', (_, res, ctx) => res(ctx.json(labels)))
+    )
+
+    expect(await tags()).to.deep.equal(labels)
+  })
+
+  it('recipes() returns a list of published cards', async () => {
+    const card1 = {
+      labels: [
+        {
+          name: 'a label',
+        },
+      ],
+    } as Recipe
+    const card2 = {
+      labels: [
+        {
+          name: 'published',
+        },
+      ],
+    } as Recipe
+    server.use(
+      rest.get(root + board + '/cards', (_, res, ctx) =>
+        res(ctx.json([card1, card2]))
+      )
+    )
+    expect(await recipes()).to.deep.equal([card2])
+  })
+
+  describe('details()', () => {
+    const card = {
+      id: '1',
+      desc: 'description',
+    }
+    const images = [
+      {
+        id: 'img1',
+        url: 'url1',
+        edgeColor: 'color1',
+        name: 'name1',
+        previews: [
+          {
+            url: 'url1-1',
+            height: 1,
+            width: 1,
+          },
+          {
+            url: 'url1-10',
+            height: 10,
+            width: 10,
+          },
+          {
+            url: 'url1-100',
+            height: 100,
+            width: 100,
+          },
+        ],
+      },
+      {
+        id: 'img2',
+        url: 'url2',
+        edgeColor: 'color2',
+        name: 'name2',
+        previews: [
+          {
+            url: 'url2-1',
+            height: 1,
+            width: 1,
+          },
+          {
+            url: 'url2-10',
+            height: 10,
+            width: 10,
+          },
+          {
+            url: 'url2-100',
+            height: 100,
+            width: 100,
+          },
+        ],
+      },
+    ]
+
+    beforeEach(() => {
+      server.use(
+        rest.get(root + '/card/1', (_, res, ctx) => res(ctx.json(card)))
+      )
+      server.use(
+        rest.get(root + '/card/1/attachments', (_, res, ctx) =>
+          res(ctx.json(images))
+        )
+      )
+    })
+
+    it('returns the extra details for a given recipe', async () => {
+      expect(await details('1')).to.deep.equal({
+        id: '1',
+        desc: 'description',
+        images: [
+          {
+            id: 'img1',
+            url: 'url1',
+            edgeColor: 'color1',
+            name: 'name1',
+          },
+          {
+            id: 'img2',
+            url: 'url2',
+            edgeColor: 'color2',
+            name: 'name2',
+          },
+        ],
+      })
+    })
+
+    it('can return the smallest scaled images that are still >= the optionally given dimensions', async () => {
+      expect(await details('1', { height: 10, width: 10 })).to.deep.equal({
+        id: '1',
+        desc: 'description',
+        images: [
+          {
+            id: 'img1',
+            url: 'url1-10',
+            edgeColor: 'color1',
+            name: 'name1',
+          },
+          {
+            id: 'img2',
+            url: 'url2-10',
+            edgeColor: 'color2',
+            name: 'name2',
+          },
+        ],
+      })
+    })
+
+    it('only requires a min height or a width to be specified', async () => {
+      expect(await details('1', { width: 100 })).to.deep.equal({
+        id: '1',
+        desc: 'description',
+        images: [
+          {
+            id: 'img1',
+            url: 'url1-100',
+            edgeColor: 'color1',
+            name: 'name1',
+          },
+          {
+            id: 'img2',
+            url: 'url2-100',
+            edgeColor: 'color2',
+            name: 'name2',
+          },
+        ],
+      })
+    })
+
+    it('must be >= both, if two dimensions are given', async () => {
+      expect(await details('1', { height: 1, width: 100 })).to.deep.equal({
+        id: '1',
+        desc: 'description',
+        images: [
+          {
+            id: 'img1',
+            url: 'url1-100',
+            edgeColor: 'color1',
+            name: 'name1',
+          },
+          {
+            id: 'img2',
+            url: 'url2-100',
+            edgeColor: 'color2',
+            name: 'name2',
+          },
+        ],
+      })
+    })
+
+    it("returns the largest available, if there isn't one any bigger", async () => {
+      expect(await details('1', { height: 101, width: 101 })).to.deep.equal({
+        id: '1',
+        desc: 'description',
+        images: [
+          {
+            id: 'img1',
+            url: 'url1-100',
+            edgeColor: 'color1',
+            name: 'name1',
+          },
+          {
+            id: 'img2',
+            url: 'url2-100',
+            edgeColor: 'color2',
+            name: 'name2',
+          },
+        ],
+      })
+    })
   })
 })
--- src/lib/data/fetch.ts
+++ src/lib/data/fetch.ts
@@ -1,5 +1,14 @@
-export const trello = (endpoint: string) => {
-  const root = 'https://api.trello.com/1/'
+export interface Tag {
+  id: string
+  idBoard: string
+  name: string
+  color: string
+}
+
+const board = '/board/5820f9c22043447d3f4fa857'
+
+const trello = <T>(endpoint: string): Promise<T> => {
+  const root = 'https://api.trello.com/1'
 
   // guard against fetch being undefined for server-side calls
   let fetch
@@ -11,18 +20,183 @@ export const trello = (endpoint: string) => {
   return fetch(root + endpoint).then((res: any) => res.json())
 }
 
-export interface Card {
-  id: string
-  name: string
-  desc: string
-  labels: Label[]
-  idAttachmentCover: string
+export const tags = (): Promise<Tag[]> => trello<Tag[]>(board + '/labels')
+
+interface MinDimensions {
+  height?: number
+  width?: number
 }
 
-interface Label {
+interface Preview {
+  url: string
+  height: number
+  width: number
+}
+
+export interface Image {
   id: string
-  idBoard: string
-  idList: string
+  edgeColor: string
+  url: string
   name: string
-  color: string
+}
+
+export interface ImageAPI extends Image {
+  previews: Array<Preview>
+}
+
+const isBigger = (
+  img: Preview,
+  { height = 0, width = 0 }: MinDimensions
+): boolean => {
+  const heightBigger = img.height >= height
+  const widthBigger = img.width >= width
+
+  return heightBigger && widthBigger
+}
+
+const findBestFit = (res: ImageAPI, minDimensions: MinDimensions): number => {
+  let index = res.previews.length - 1
+
+  // search previews for an image that's scaled to being only as
+  // big as is needed
+  res.previews.some((preview, current) => {
+    const bigger = isBigger(preview, minDimensions)
+
+    // when the first preview not bigger is found, store previous
+    // index for extracting the preview's url
+    if (bigger) {
+      index = current
+    }
+
+    // then return bigger to short circuit if true,
+    // or continue if false
+    return bigger
+  })
+
+  return index
+}
+
+export const image = (
+  cardId: string,
+  imageId: string,
+  minDimensions: MinDimensions | null = null
+): Promise<Image> => {
+  if (minDimensions) {
+    if (minDimensions.height === null && minDimensions.width === null) {
+      throw TypeError(
+        `at least one property on minDimensions must be provided: ${minDimensions}`
+      )
+    }
+  }
+
+  const api = trello<ImageAPI>(
+    `/card/${cardId}/attachments/${imageId}?fields=id,name,url,previews`
+  )
+
+  // FIXME: I'm not sure this part is even working, but it needs pulled out
+  // into a separate function so it can be used in recipeDetails() as well
+  if (minDimensions)
+    return api.then((res) => {
+      const index = findBestFit(res, minDimensions)
+
+      return {
+        url: res.previews[index].url,
+        id: res.id,
+        edgeColor: res.edgeColor,
+        name: res.name,
+      }
+    })
+  else
+    return api.then((res) => {
+      return {
+        url: res.url,
+        id: res.id,
+        edgeColor: res.edgeColor,
+        name: res.name,
+      }
+    })
+}
+
+export interface Recipe {
+  id: string
+  name: string
+  labels: Array<Tag>
+  coverImage: Image
+  idList: string
+}
+
+interface RecipeAPI {
+  id: string
+  name: string
+  labels: Array<Tag>
+  idAttachmentCover: string
+  idList: string
+}
+
+export const recipes = (): Promise<RecipeAPI[]> => {
+  const filterPublished = (cards: RecipeAPI[]) => {
+    let filtered: RecipeAPI[] = []
+
+    cards.forEach((card) => {
+      card.labels.some((label) => label.name === 'published')
+        ? filtered.push(card)
+        : null
+    })
+
+    return filtered
+  }
+
+  return trello<RecipeAPI[]>(
+    board + '/cards?fields=id,name,idList,labels,idAttachmentCover'
+  ).then(filterPublished)
+}
+
+export interface RecipeAPIDetails {
+  id: string
+  desc: string
+}
+
+export interface RecipeDetails extends RecipeAPIDetails {
+  images: Array<Image>
+}
+
+export const details = async (
+  id: string,
+  minDimensions: MinDimensions | null = null
+): Promise<RecipeDetails> => {
+  const card = await trello<RecipeAPIDetails>(`/card/${id}?fields=id,desc`)
+  const imagesAPI = trello<ImageAPI[]>(
+    `/card/${id}/attachments?fields=id,name,url,previews`
+  )
+
+  let images: Image[]
+
+  if (minDimensions)
+    images = await imagesAPI.then((results) =>
+      results.map((res) => {
+        const index = findBestFit(res, minDimensions)
+
+        return {
+          url: res.previews[index].url,
+          id: res.id,
+          edgeColor: res.edgeColor,
+          name: res.name,
+        }
+      })
+    )
+  else
+    images = await imagesAPI.then((results) =>
+      results.map((res) => ({
+        url: res.url,
+        id: res.id,
+        edgeColor: res.edgeColor,
+        name: res.name,
+      }))
+    )
+
+  return {
+    images: images,
+    id: card.id,
+    desc: card.desc,
+  }
 }
