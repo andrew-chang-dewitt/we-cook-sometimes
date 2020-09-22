8549ed278a76306abcb77eeee6d839dd2bf8aa41
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-09-22 07:47:48 -0400

    root includes List

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -2,17 +2,21 @@ import React from 'react'
 
 import useStateHistory from '../utils/useStateHistory'
 import fetch from '../lib/data/fetch'
+// import fetch, { Tag as TagType } from '../lib/data/fetch'
 import RecipeList, {
   RecipeList as RecipeListType,
 } from '../lib/core/RecipeList'
 
-import styles from './Root.module.sass'
+import List from './recipes/List'
+
+// import styles from './Root.module.sass'
 
 const publishedTagId = '5f55960c17f08e1fde18785e'
 
 const Root = () => {
   const { state, setState } = useStateHistory({
     recipes: {} as RecipeListType,
+    // tags: {} as TagType[],
   })
 
   // calling setState later allows for component to load while
@@ -22,22 +26,32 @@ const Root = () => {
   // infinite loop as the component is reloaded because
   // setState was called
   React.useEffect(() => {
+    // Fetch Recipes from Trello API, then build Recipe List from lib
     fetch
       .recipes()
       .then((res) =>
         setState({
+          ...state,
           recipes: RecipeList.create(res.unwrap()).filterByTag(publishedTagId),
         })
       )
       .catch((e) => {
         throw e
       })
+
+    // // Fetch Tags from Trello API
+    // fetch.tags().then((res) => setState({ ...state, tags: res.unwrap() }))
   }, [])
 
   return (
     <>
-      <h1 className={styles.title}>What about this?</h1>
-      <div>{JSON.stringify(state.recipes.remaining)}</div>
+      {state.recipes.remaining ? (
+        <List
+          recipes={state.recipes.remaining.map(
+            (recipe) => state.recipes.allByID[recipe]
+          )}
+        />
+      ) : null}
     </>
   )
 }
