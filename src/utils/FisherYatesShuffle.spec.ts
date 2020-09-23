2a1295cd7ef1cc40c67166193db6e3dcf3e30a67
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-09-23 16:19:20 -0400

    a Fisher-Yates shuffle implementation

---
+++ src/utils/FisherYatesShuffle.spec.ts
@@ -0,0 +1,29 @@
+import 'mocha'
+import { expect } from 'chai'
+
+import shuffle from './FisherYatesShuffle'
+
+describe('src/utils/FisherYatesShuffle', () => {
+  it('returns a shuffled copy of a given array', () => {
+    let fakeRandomNumbers = [4 / 5, 1 / 4, 1 / 3, 0, 1][Symbol.iterator]()
+
+    /*
+     * range  rand  scratch       result
+     *              0,1,2,3,4,5
+     * 0-5    4     0,1,2,3,5     4
+     * 0-4    1     0,5,2,3       1,4
+     * 0-3    1     0,3,2         5,1,4
+     * 0-2    0     2,3           0,5,1,4
+     * 0-1    1     2             3,0,5,1,4
+     *
+     */
+
+    const rand = (): number => {
+      const res = fakeRandomNumbers.next().value
+      console.log('random number:', res)
+      return res
+    }
+
+    expect(shuffle([0, 1, 2, 3, 4, 5], rand)).to.deep.equal([2, 3, 0, 5, 1, 4])
+  })
+})
