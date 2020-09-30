659ba9a53591bd8524bbd828b6ee20ec43680b6f
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-09-29 20:20:09 -0400

    ignoring for test coverage, no logic to test

--- src/components/icons/More.tsx
+++ src/components/icons/More.tsx
@@ -1,3 +1,7 @@
+// there's 0 logic here, this is simply just an encapsulation
+// of a reusable see more (vertical ellipsis) icon, no need to
+// any of it
+/* istanbul ignore file */
 import React from 'react'
 
 import styles from './More.module.sass'
