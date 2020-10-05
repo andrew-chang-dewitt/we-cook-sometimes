b94770fef97b67e1f7032650df8183808d4b96d2
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-10-05 11:17:56 -0400

    prettier --fix

--- src/components/icons/Close.tsx
+++ src/components/icons/Close.tsx
@@ -1,5 +1,5 @@
 // there's 0 logic here, this is simply just an encapsulation
-// of a reusable see more (vertical ellipsis) icon, no need to 
+// of a reusable see more (vertical ellipsis) icon, no need to
 // any of it
 /* istanbul ignore file */
 import React from 'react'
