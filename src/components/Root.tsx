cd8ca4261db819a11a6726154b690c5c33c90b59
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-10-05 16:54:26 -0400

    fixing lookup context provider

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -6,20 +6,39 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import shuffle from '../utils/FisherYatesShuffle'
 
 // core logic
-import fetch, { Recipe } from '../lib/data/fetch'
+import fetch from '../lib/data/fetch'
 import questions from '../lib/data/questions'
+import RecipeList, {
+  RecipeList as RecipeListType,
+} from '../lib/core/RecipeList'
 
 // other components
+import LookupContext, { LookupTables, RecipeLookup, RecipeByID } from '../utils/LookupContext'
 import Header from './header/Header'
 import Home from './home/Home'
 import RecipePage from './RecipePage'
 
 // import styles from './Root.module.sass'
 
-const Root = () => {
+const buildByUrlTable = (recipeList: RecipeListType): RecipeLookup => {
+  const byUrl: RecipeLookup = {}
+
+  Object.values(recipeList.allByID).forEach(
+    (recipe) => (byUrl[recipe.shortLink] = recipe.id)
+  )
+
+  return byUrl
+}
+
+export default () => {
   const [recipes, setRecipes] = React.useState({
-    data: [] as Array<Recipe>,
+    data: {} as RecipeListType,
     loaded: false,
+  })
+
+  const [ lookupTables, setLookupTables ] = React.useState<LookupTables>({
+    recipeByUrl: {} as RecipeLookup,
+    recipeByID: {} as RecipeByID
   })
 
   // calling setState later allows for component to load while
@@ -33,7 +52,12 @@ const Root = () => {
     fetch
       .recipes()
       .then((res) => {
-        const recipeList = shuffle(res.unwrap(), Math.random)
+        const recipeList = RecipeList.create(shuffle(res.unwrap(), Math.random))
+
+        setLookupTables({
+          recipeByUrl: buildByUrlTable(recipeList),
+          recipeByID: recipeList.allByID
+        })
 
         setRecipes({
           data: recipeList,
@@ -47,6 +71,7 @@ const Root = () => {
   }, [])
 
   return (
+    <LookupContext.Provider value={lookupTables}>
     <Router>
       <Header />
       <Switch>
@@ -63,7 +88,6 @@ const Root = () => {
         </Route>
       </Switch>
     </Router>
+    </LookupContext.Provider>
   )
 }
-
-export default Root
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,49 +1,33 @@
 import React from 'react'
 
 // core logic
-import { publishedTagId, Recipe } from '../../lib/data/fetch'
+import { publishedTagId} from '../../lib/data/fetch'
 import {
   Question as QuestionType,
   Choice as ChoiceType,
 } from '../../lib/data/questions'
 import QuestionSeries from '../../lib/core/QuestionSeries'
-import RecipeList, {
+import {
   RecipeList as RecipeListType,
 } from '../../lib/core/RecipeList'
 
 // internal utilities
 import useStateHistory from '../../utils/useStateHistory'
-import LookupContext, { RecipeLookup } from '../../utils/LookupContext'
 
 // other components
 import Question from './questions/Question'
 import List from './recipes/List'
 
 interface Props {
-  recipes: Array<Recipe>
+  recipes: RecipeListType
   questions: Array<QuestionType>
-}
-
-const buildByUrlTable = (recipeList: RecipeListType): RecipeLookup => {
-  const byUrl: RecipeLookup = {}
-
-  Object.values(recipeList.allByID).forEach(
-    (recipe) => (byUrl[recipe.shortLink] = recipe.id)
-  )
-
-  return byUrl
 }
 
 export default ({ recipes, questions }: Props) => {
   const { state, setState, undo, onOldest } = useStateHistory({
-    recipes: RecipeList.create(recipes).filterByTag(publishedTagId),
+    recipes: recipes.filterByTag(publishedTagId),
     questions: QuestionSeries.create(questions),
   })
-  const lookupTables = {
-    recipeByUrl: buildByUrlTable(state.recipes),
-    recipeByID: state.recipes.allByID,
-  }
-
   const answerQuestion = (answer: ChoiceType) => {
     setState({
       recipes: state.recipes.eliminateByTags(answer.tagsEliminated),
@@ -52,7 +36,7 @@ export default ({ recipes, questions }: Props) => {
   }
 
   return (
-    <LookupContext.Provider value={lookupTables}>
+    <>
       {state.questions.current ? (
         <Question
           question={state.questions.current}
@@ -67,6 +51,6 @@ export default ({ recipes, questions }: Props) => {
           (recipe) => state.recipes.allByID[recipe]
         )}
       />
-    </LookupContext.Provider>
+    </>
   )
 }
