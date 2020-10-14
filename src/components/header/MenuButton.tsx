4f0ee6644983f231d4acd1d80454aa53f24b73bd
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-10-13 20:43:53 -0400

    Adding a navigation menu as a route on `/menu`
    Simple navigation menu with Home, All Recipes, & a search bar that
    navigates to all-recipes with the search query completed.

--- src/components/header/MenuButton.tsx
+++ src/components/header/MenuButton.tsx
@@ -1,19 +1,36 @@
 // external dependencies
 import React from 'react'
+import { Link, useLocation, useHistory } from 'react-router-dom'
 
 // other components
 import HamburgerIcon from '../icons/HamburgerIcon'
 
-// CSS-modules
-// import styles from './BackButton.module.sass'
+export default () => {
+  const [open, setOpen] = React.useState(false)
+  const history = useHistory()
+  const location = useLocation()
 
-interface Props {
-  open: boolean
-  handler: () => void
+  const closeMenuButtonHandler = (): void => {
+    history.goBack()
+  }
+
+  const menuClosed = (
+    <button aria-label="Open navigation menu">
+      <Link to="/menu">
+        <HamburgerIcon open={false} />
+      </Link>
+    </button>
+  )
+
+  const menuOpened = (
+    <button aria-label="Close navigation Menu" onClick={closeMenuButtonHandler}>
+      <HamburgerIcon open={true} />
+    </button>
+  )
+
+  React.useEffect(() => {
+    location.pathname === '/menu' ? setOpen(true) : setOpen(false)
+  }, [location])
+
+  return open ? menuOpened : menuClosed
 }
-
-export default ({ open, handler }: Props) => (
-  <button aria-label="Navigation Menu" onClick={handler}>
-    <HamburgerIcon open={open} />
-  </button>
-)
