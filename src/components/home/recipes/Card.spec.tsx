8c2abdb3067a58851b29d803226d2694f3d3f9ef
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-10-14 11:13:04 -0400

    Better test coverage & fixing broken tests
    Adds coverage for handler delegation & fixes tests on recipe name
    rendering to account for indication of WIP or not.

--- src/components/home/recipes/Card.spec.tsx
+++ src/components/home/recipes/Card.spec.tsx
@@ -1,19 +1,21 @@
+// testing tools
 import 'mocha'
 import { expect } from 'chai'
 import { shallow, configure, ShallowWrapper } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-
 configure({ adapter: new Adapter() })
-
 import React from 'react'
 
-import { Tag as TagData } from '../../../lib/data/fetch'
+// internal dependencies
+import { publishedTagId, Tag as TagType } from '../../../lib/data/fetch'
 import TagList from '../../tags/List'
 import ImageLoader from '../../images/ImageLoader'
 import DetailLoader from '../../detail/DetailLoader'
 import More from '../../icons/More'
 import Close from '../../icons/Close'
+import WIP from '../../WIPTag'
 
+// component under test
 import Card from './Card'
 
 describe('src/component/recipes/Card', () => {
@@ -22,9 +24,9 @@ describe('src/component/recipes/Card', () => {
     name: 'A Recipe',
     shortLink: 'a url',
     tags: [
-      ('1' as any) as TagData,
-      ('2' as any) as TagData,
-      ('3' as any) as TagData,
+      ('1' as any) as TagType,
+      ('2' as any) as TagType,
+      ('3' as any) as TagType,
     ],
     idList: 'a list',
     idAttachmentCover: 'img',
@@ -39,7 +41,7 @@ describe('src/component/recipes/Card', () => {
   })
 
   it('renders the recipe title', () => {
-    expect(card.find('h3').text()).to.equal('A Recipe')
+    expect(card.find('h3').text()).to.match(/A Recipe/)
   })
 
   it('renders a list of Tags', () => {
@@ -65,6 +67,32 @@ describe('src/component/recipes/Card', () => {
     expect(card.find(More)).to.have.lengthOf(1)
   })
 
+  describe('work in progress', () => {
+    it("indicates if a recipe isn't tagged as published", () => {
+      const wip = {
+        ...recipe,
+        tags: [],
+      }
+      const rendered = shallow(
+        <Card recipe={wip} detailsOpen={false} openHandler={() => {}} />
+      )
+
+      expect(rendered.find('h3').contains(<WIP />)).to.be.true
+    })
+
+    it("indicates if a recipe isn't tagged as published", () => {
+      const wip = {
+        ...recipe,
+        tags: [{ id: publishedTagId } as TagType],
+      }
+      const rendered = shallow(
+        <Card recipe={wip} detailsOpen={false} openHandler={() => {}} />
+      )
+
+      expect(rendered.find('h3').contains(<WIP />)).to.be.false
+    })
+  })
+
   describe('open details', () => {
     before(() => {
       card = shallow(
@@ -76,8 +104,48 @@ describe('src/component/recipes/Card', () => {
       expect(card.find(DetailLoader)).to.have.lengthOf(1)
     })
 
-    it('and it displays an x icon indicating the details can be closed', () => {
+    it('displays an x icon indicating the details can be closed', () => {
       expect(card.find(Close)).to.have.lengthOf(1)
+    })
+
+    describe('delegates a user clicking the x or ellipsis buttons to the consumer', () => {
+      it('passes the recipe ID to the handler if the card is not yet open', () => {
+        let handlerCalledWith = ''
+
+        card = shallow(
+          <Card
+            recipe={recipe}
+            detailsOpen={false}
+            openHandler={(arg: string) => {
+              console.log('handler called')
+              handlerCalledWith = arg
+            }}
+          />
+        )
+
+        card.find('a').simulate('click', { preventDefault: () => {} })
+
+        expect(handlerCalledWith).to.equal(recipe.id)
+      })
+
+      it('passes an empty string to the handler if the card is already open', () => {
+        let handlerCalledWith = 'an id'
+
+        card = shallow(
+          <Card
+            recipe={recipe}
+            detailsOpen={true}
+            openHandler={(arg: string) => {
+              console.log('handler called')
+              handlerCalledWith = arg
+            }}
+          />
+        )
+
+        card.find('a').simulate('click', { preventDefault: () => {} })
+
+        expect(handlerCalledWith).to.equal('')
+      })
     })
   })
 })
