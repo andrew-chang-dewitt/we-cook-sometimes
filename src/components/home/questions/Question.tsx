ec887012ab477f9993533a178bfa8f93b94d926c
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-10-19 16:45:58 -0400

    Better styles & allows collapsing of question
    - Updates styles of question & header to include background so that List
      elements end up hidden behind them on scroll.
    - Also adds ability to collapse question to free up more screen real
      estate

--- src/components/home/questions/Question.tsx
+++ src/components/home/questions/Question.tsx
@@ -1,9 +1,16 @@
+// external libraries
 import React from 'react'
 
+// core logic
 import { Question, Choice as ChoiceType } from '../../../lib/data/questions'
 
+// other components
+import LayoutContext from '../../LayoutContext'
 import Choice from './Choice'
+import Up from '../../icons/Up'
+import Down from '../../icons/Down'
 
+// CSS-modules
 import styles from './Question.module.sass'
 
 interface Props {
@@ -22,42 +29,86 @@ export default ({
   previous,
   previousExists,
   reset,
-}: Props) => (
-  <div className={styles.container}>
-    {question ? (
-      <>
-        <h2 className={styles.question}>{question.text}</h2>
-        <ul>
-          {question.choices.map((choice) => (
-            <Choice
-              key={choice.text}
-              data={choice}
-              handler={() => submitAnswer(choice)}
-            />
-          ))}
-        </ul>
-      </>
-    ) : (
-      <h2 className={styles.question}>...no more questions</h2>
-    )}
-    <div className={styles.questionNavigation}>
-      {previousExists ? (
-        <>
-          <button onClick={previous}>Previous question</button>
-          <button onClick={reset}>Start over</button>
-        </>
-      ) : null}
-    </div>
-    <hr />
-    {submittedAnswers.length > 0 ? (
-      <div className={styles.submittedAnswers}>
-        <p>Filtering by:</p>
-        <ul>
-          {submittedAnswers.map((answer) => (
-            <li key={answer}>{answer}</li>
-          ))}
-        </ul>
+}: Props) => {
+  const [questionCollapsed, setQuestionsCollapsed] = React.useState(false)
+  const setYOffset = React.useContext(LayoutContext).setScrollYOffset
+
+  const openQuestion = () => {
+    setQuestionsCollapsed(false)
+    setYOffset(270)
+  }
+  const closeQuestion = () => {
+    setQuestionsCollapsed(true)
+    setYOffset(52)
+  }
+
+  React.useEffect(() => {
+    setYOffset(270)
+  }, [])
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.container}>
+        <div
+          className={`${styles.questionContainer} ${
+            questionCollapsed ? styles.collapsed : ''
+          }`}
+        >
+          {question ? (
+            <>
+              <h2 className={styles.question}>{question.text}</h2>
+
+              <ul>
+                {question.choices.map((choice) => (
+                  <Choice
+                    key={choice.text}
+                    data={choice}
+                    handler={() => submitAnswer(choice)}
+                  />
+                ))}
+              </ul>
+            </>
+          ) : (
+            <h2 className={styles.question}>...no more questions</h2>
+          )}
+
+          <div className={styles.questionNavigation}>
+            {previousExists ? (
+              <>
+                <button onClick={previous}>Previous question</button>
+
+                <button onClick={reset}>Start over</button>
+              </>
+            ) : null}
+          </div>
+        </div>
+
+        <div className={styles.divider}>
+          <hr />
+
+          {questionCollapsed ? (
+            <button onClick={openQuestion}>
+              <Down />
+            </button>
+          ) : (
+            <button onClick={closeQuestion}>
+              <Up />
+            </button>
+          )}
+        </div>
+
+        {submittedAnswers.length > 0 ? (
+          <div className={styles.submittedAnswers}>
+            <p>Filtering by:</p>
+
+            <ul>
+              {submittedAnswers.map((answer) => (
+                <li key={answer}>{answer}</li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
       </div>
-    ) : null}
-  </div>
-)
+    </div>
+  )
+}
---
+++ src/components/icons/Down.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './NextPrevious.module.sass'
+
+export default () => (
+  <svg className={styles.svgIcon}>
+    <use xlinkHref="/static/icons.svg#chevron-down" />
+  </svg>
+)
