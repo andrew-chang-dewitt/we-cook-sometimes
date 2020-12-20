581a1027c5f72e8b2c70a9df943de4cb69452bd3
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-12-20 16:29:56 -0500

    Create new Tag lookup table

--- src/utils/LookupContext.ts
+++ src/utils/LookupContext.ts
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { RecipeCard } from '../lib/data/schema'
+import { RecipeCard, Tag } from '../lib/data/schema'
 
 export interface RecipeLookup {
   [key: string]: string
@@ -8,12 +8,17 @@ export interface RecipeLookup {
 export interface RecipeByID {
   [key: string]: RecipeCard
 }
+export interface TagByID {
+  [key: string]: Tag
+}
 export interface LookupTables {
   recipeByUrl: RecipeLookup
   recipeByID: RecipeByID
+  tagsByID: TagByID
 }
 
 export default React.createContext<LookupTables>({
   recipeByUrl: {},
   recipeByID: {},
+  tagsByID: {},
 })
