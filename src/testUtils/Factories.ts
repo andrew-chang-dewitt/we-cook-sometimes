7bc84622998e6d1b109f19c16649003f5232aa1f
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2020-12-21 11:21:37 -0500

    Remove unecessary factories

--- src/testUtils/Factories.ts
+++ src/testUtils/Factories.ts
@@ -1,6 +1,5 @@
 /* istanbul ignore file */
 
-import * as fetchLib from '../lib/data/fetch'
 import * as schemaLib from '../lib/data/schema'
 import {
   exclusionary,
@@ -117,111 +116,6 @@ export class RecipeDetails {
   }
 }
 
-export const fetch = {
-  API: {
-    Image: {
-      create: (): fetchLib.ImageAPI => ({
-        id: '1',
-        name: '[published]name',
-        edgeColor: 'color',
-        url: 'url',
-        previews: [
-          {
-            height: 1,
-            width: 1,
-            url: 'url1',
-          },
-          {
-            height: 10,
-            width: 10,
-            url: 'url10',
-          },
-          {
-            height: 100,
-            width: 100,
-            url: 'url100',
-          },
-        ],
-      }),
-
-      createWithId: (id: string): fetchLib.ImageAPI => {
-        const img = fetch.API.Image.create()
-        img.id = id
-
-        return img
-      },
-
-      createWithProperties: (
-        properties: { key: string; value: any }[]
-      ): fetchLib.ImageAPI => {
-        const img = fetch.API.Image.create()
-
-        properties.map(({ key, value }) => {
-          img[key] = value
-        })
-
-        return img
-      },
-    },
-
-    Recipe: {
-      create: (): fetchLib.RecipeAPI => ({
-        id: 'id',
-        name: 'one',
-        shortLink: 'https://a-link',
-        idAttachmentCover: 'img',
-        idList: 'list',
-        labels: [
-          fetch.API.Tag.createWithData({
-            id: 'labelATag',
-            name: 'a tag',
-          }),
-        ],
-      }),
-
-      createWithProperties: (
-        properties: { key: string; value: any }[]
-      ): fetchLib.RecipeAPI => {
-        const recipe = fetch.API.Recipe.create()
-
-        properties.map(({ key, value }) => {
-          recipe[key] = value
-        })
-
-        return recipe
-      },
-    },
-
-    Tag: {
-      createWithData: (data: { id: string; name: string }): fetchLib.Tag =>
-        data as fetchLib.Tag,
-    },
-  },
-
-  Recipe: {
-    create: (): fetchLib.Recipe =>
-      ({
-        id: 'recipeId',
-        name: 'recipeName',
-        tags: [
-          fetch.API.Tag.createWithData({
-            id: 'tagId',
-            name: 'tag',
-          }),
-        ],
-      } as fetchLib.Recipe),
-
-    createWithProperties: (properties: any): fetchLib.Recipe => {
-      const recipe = fetch.Recipe.create()
-
-      return {
-        ...recipe,
-        ...(properties as fetchLib.Recipe),
-      }
-    },
-  },
-}
-
 interface QuestionProps {
   id?: string
   text?: string
@@ -325,6 +219,5 @@ export const questions = {
 
 export default {
   questions,
-  fetch,
   schema: { Tag, Image, RecipeCard, RecipeDetails },
 }
